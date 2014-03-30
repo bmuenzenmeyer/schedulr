56e293c5ecdbe2a00579ce67b604be3a316e7250
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2014-03-30 07:21:22 -0500

    Lots of enhancements
    Tracks employee total hours fixes #2
    Shift types fixes #4
    Edit or delete a shift fixes #11

--- src/js/schedulr.js
+++ src/js/schedulr.js
@@ -8,7 +8,10 @@ app.controller('SchedulrCtrl', function($scope) {
   $scope.shiftBuilderName = '';
   $scope.shiftBuilderStart = '';
   $scope.shiftBuilderEnd = '';
+  $scope.shiftBuilderDesc = '';
   $scope.unScheduledShifts = [];
+  $scope.employees = {};
+  $scope.hourError = false;
 
   $scope.$watch('weekStart', function(newValue, oldValue){
     if(newValue !== oldValue){
@@ -30,6 +33,30 @@ app.controller('SchedulrCtrl', function($scope) {
     connectWith: ".shiftList"
   };
 
+  function getShiftLength(start, end){
+
+    var safeStart = parseInt(start, 0);
+    var safeEnd = parseInt(end, 0);
+
+    var shiftLength = 0;
+    var additionalHours = 0;
+    if(!isNaN(safeStart) && !isNaN(safeEnd)){
+      $scope.hourError = false;
+      if(safeStart > safeEnd){
+        additionalHours = 12 - safeStart;
+        shiftLength = additionalHours + safeEnd;
+      } else if(safeStart < safeEnd){
+        shiftLength = safeEnd - safeStart;
+      } else{
+        shiftLength = 12;
+      }
+    } else{
+      //add notifcation to client
+      $scope.hourError = true;
+    }
+    return shiftLength;
+  }
+
   $scope.buildDays = function(){
     $scope.days.length = 0;
     var m = moment($scope.weekStart);
@@ -47,26 +74,66 @@ app.controller('SchedulrCtrl', function($scope) {
   };
 
   $scope.addShift = function(){
+
+    var shiftLength = getShiftLength($scope.shiftBuilderStart, $scope.shiftBuilderEnd);
+
+    if($scope.hourError){
+      return;
+    }
     $scope.unScheduledShifts.push({
       empName : $scope.shiftBuilderName,
       start: $scope.shiftBuilderStart,
-      end: $scope.shiftBuilderEnd
+      end: $scope.shiftBuilderEnd,
+      desc: $scope.shiftBuilderDesc,
+      editing: false,
+      shiftLength: shiftLength
     });
+
+    if($scope.employees[$scope.shiftBuilderName]){
+      $scope.employees[$scope.shiftBuilderName].totalHours += shiftLength;
+    } else{
+      $scope.employees[$scope.shiftBuilderName] = {totalHours: shiftLength};
+    }
 
     $scope.shiftBuilderName = '';
     $scope.shiftBuilderStart = '';
     $scope.shiftBuilderEnd = '';
-
-    $scope.initSortable();
+    $scope.shiftBuilderDesc = '';
 
   };
- 
-  // $scope.remaining = function() {
-  //   var count = 0;
-  //   angular.forEach($scope.todos, function(todo) {
-  //     count += todo.done ? 0 : 1;
-  //   });
-  //   return count;
-  // };
+
+  $scope.saveShift = function(shift, day){
+
+    //todo - recalculate hours
+    shift.editing = false;
+  };
+
+  $scope.deleteShift = function(shift, day){
+    var confirmDelete = confirm('Are you sure you want to delete this shift? Click Ok to delete.');
+    if(!confirmDelete){
+      return;
+    }
+    var d = $scope.days[$scope.days.indexOf(day)];
+
+    //remove shiftLength from employee
+    $scope.employees[shift.empName].totalHours -= shift.shiftLength;
+
+    shift.editing = false;
+    d.shifts.splice(d.shifts.indexOf(shift),1);
+  };
+
+  $scope.editShift = function(shift){
+    shift.editing = true;
+  };
+
+  $scope.doneEditing = function(shift){
+    shift.editing = false;
+  };
+
+  // //quicker debugging
+  //   $scope.shiftBuilderName = 'Megan';
+  //   $scope.shiftBuilderStart = '9';
+  //   $scope.shiftBuilderEnd = '5';
+  //   $scope.shiftBuilderDesc = 'Cooking';
 
 });
